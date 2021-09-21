f598bdb09f6001c3de9fe45856b3b3a02d676c03
sandeepan swain <sandeepan.swain@people10.com>
2021-09-21 17:31:12 +0530

    Initial Commit

---
+++ ZivameTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZivameTask
+//
+//  Created by Sandeepan Swain on 21/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
